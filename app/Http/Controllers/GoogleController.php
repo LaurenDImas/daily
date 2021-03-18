0859e24eecb9e55f1a5394a786f5e59fe10a1973
dimas yogi <dimas.yogi.777@gmail.com>
2021-03-19 00:57:14 +0700

    first commit

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
-use App\user;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class GoogleController extends Controller
